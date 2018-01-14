97609bf8461de17cd622b44a7f2260fb1d7b603a
Dipesh-Budhiraja <25dipesh98@gmail.com>
2018-01-14 23:56:11 +0530

    phew

--- app.js
+++ app.js
@@ -1,97 +1,91 @@
 const express = require('express');
-const exphbs  = require('express-handlebars');
 const path = require('path');
-const mongoose = require('mongoose');
-var bodyParser = require('body-parser');
+const exphbs  = require('express-handlebars');
 const methodOverride = require('method-override');
 const flash = require('connect-flash');
 const session = require('express-session');
+const bodyParser = require('body-parser');
 const passport = require('passport');
+const mongoose = require('mongoose');
 
 const app = express();
 
-//load routes
+// Load routes
 const ideas = require('./routes/ideas');
 const users = require('./routes/users');
 
-//passport config
+// Passport Config
 require('./config/passport')(passport);
-
-//db config
+// DB Config
 const db = require('./config/database');
 
-//connect to mongoose
+// Map global promise - get rid of warning
 mongoose.Promise = global.Promise;
+// Connect to mongoose
+mongoose.connect(db.mongoURI, {
+  useMongoClient: true
+})
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
 
-// mongoose.connect('mongodb://localhost/vidjot-dev', {useMongoClient: true})
-mongoose.connect(db.mongoURI, {useMongoClient: true})
-.then(() => { console.log('Mongodb connected');})
-.catch((err) => {
-    console.log(err);
-});
-
-
-//how middleware use
-// app.use(function(req, res, next){
-//     // console.log(Date.now());
-//     // req.name = 'dipesh';
-//     next();
-// });
-
-//middlewares
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+// Handlebars Middleware
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
 app.set('view engine', 'handlebars');
 
-//body parsa
+// Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-//static folder
+// Static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-// method override middleware
+// Method override middleware
 app.use(methodOverride('_method'));
 
-//express session middleware
+// Express session midleware
 app.use(session({
-    secret: 'keyboardo cats',
-    resave: true,
-    saveUninitialized: true
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true
 }));
 
-//passport middleware
+// Passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(flash());
 
-//global variables
+// Global variables
 app.use(function(req, res, next){
-    res.locals.success_msg = req.flash('success_msg');
-    res.locals.error_msg = req.flash('error_msg');
-    res.locals.error = req.flash('error');
-    res.locals.user = req.user || null;
-    next();
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
+  next();
 });
 
+// Index Route
 app.get('/', (req, res) => {
-    // console.log(req.name);
-    const title = 'welcome';
-    res.render('INDEX', {title: title});
+  const title = 'Welcome';
+  res.render('index', {
+    title: title
+  });
 });
 
-//about route
+// About Route
 app.get('/about', (req, res) => {
-    res.render('ABOUT');
+  res.render('about');
 });
 
-//use routes
+
+// Use routes
 app.use('/ideas', ideas);
 app.use('/users', users);
 
-const port =  process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-    console.log(`server running on port ${port}`);
-    
+app.listen(port, () =>{
+  console.log(`Server started on port ${port}`);
 });
