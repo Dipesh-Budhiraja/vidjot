00284c3ee4c95fbf9942c49b0120a92dfd5e0356
Dipesh-Budhiraja <25dipesh98@gmail.com>
2018-01-14 23:33:48 +0530

    modified gitignore

---
+++ config/database.js
@@ -0,0 +1,5 @@
+if(process.env.NODE_ENV === 'production'){
+    module.exports = {mongoURI: 'mongodb://admin:admin@ds249727.mlab.com:49727/vidjot'}
+}else{
+    module.exports = {mongoURI: 'mongodb://localhost/vidjot-dev'}
+}
